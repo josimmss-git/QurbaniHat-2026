3aa4e1d15668bc0fc459d500ac826e45366e1b02
josimmss-git <josimmss2021@gmail.com>
2026-05-24 21:06:12 +0600

    added features sectionaccording to requrement

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,27 +1,24 @@
 
 import React from 'react';
+import PhotoCard from './PhotoCard';
 
 const FreaturePage = async () => {
   const res = await fetch('https://qurbani-hat-2026.vercel.app/Data.json')
   const photos = await res.json()
-  const topphotos=photos.animals.slice(0,6)
+  const topphotos=photos.animals.slice(3,7)
 
   console.log(topphotos);
 
 
   return (
     <div>
-      <h2>Featured Animals</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-        {topphotos.map((photo) => (
-          <div key={photo.id} className='border rounded-lg p-4'>
-            <img src={photo.image} alt={photo.name} className='w-full'/>
-            <h3 className='text-xl font-bold mb-2'>{photo.name}</h3>
-            <p className='text-gray-600 mb-2'>Age: {photo.age} years</p>
-            <p className='text-gray-600 mb-2'>Location: {photo.location}</p>
-            <p className='text-gray-600'>{photo.description}</p>
-          </div>
-        ))}
+     <h2 className='text-2xl font-bold m-4 flex text-center justify-center'>Featured Animals</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-4 p-4'>
+        {
+          topphotos.map((photo) => (
+            <PhotoCard key={photo.id} photo={photo} />
+          ))
+        }
       </div>
     </div>
   );
